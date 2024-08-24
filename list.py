247ca871dd29597be7f5ea6c23bbd57e2a686df5
Ann <kairuthiann2@gmail.com>
2024-08-24 23:46:05 +0300

    Adding list operations assignment

---
+++ list.py
@@ -0,0 +1,23 @@
+# Empty list
+my_list = []
+
+# Append elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert a value to the list
+my_list.insert(1, 15)
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in the list
+index_of = my_list.index(30)
+print(index_of)
+
+
